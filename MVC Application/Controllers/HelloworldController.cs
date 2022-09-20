b62481ddbbee53adeba5f4dbe881e098650e24ee
W0794666 <W0794666@ADSOL0214Z-33>
2022-09-20 11:44:08 -0400

    ADD

--- MVC Application/Controllers/HelloworldController.cs
+++ MVC Application/Controllers/HelloworldController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Text.Encodings.Web;
 namespace MVC_Application.Controllers
 {
     public class HelloworldController : Controller
     {
-        public IActionResult Index()
+        /*        public IActionResult Index()
+                {
+                    return View();
+                }*/
+
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
         }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+
     }
 }
--- MVC Application/Readme.txt
+++ MVC Application/Readme.txt
@@ -1,2 +1,13 @@
 ﻿2022-09-20 
 Spun up a defauklt asp.net MVC Application
+Config 
+.Net
+
+
+
+
+
+
+
+1048 Confirm That I finsihed part1
+1048 
